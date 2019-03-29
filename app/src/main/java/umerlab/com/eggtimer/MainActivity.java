640619eec19c840f19a0dc0484ceac72ea4a8291
Dr.Faheem Chishti <umerlab@gmail.com>
2019-03-29 09:17:26 +0500

    Initial commit

---
+++ app/src/main/java/umerlab/com/eggtimer/MainActivity.java
@@ -0,0 +1,96 @@
+package umerlab.com.eggtimer;
+
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView timerTextView;
+    SeekBar timerSeekBar;
+    Boolean counterIsactive = false;
+    Button goButton;
+    CountDownTimer countDownTimer;
+
+    public void buttonClicked(View view){
+        if (counterIsactive){
+            resetTimer();
+        }else{
+            counterIsactive = true;
+            timerSeekBar.setEnabled(false);
+            goButton.setText("STOP!");
+
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress()*1000+100, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTimer((int)millisUntilFinished / 1000 );
+                }
+
+                @Override
+                public void onFinish() {
+                    MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.service_bell);
+                    mPlayer.start();
+                    resetTimer();
+                }
+            }.start();
+
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timerSeekBar = findViewById(R.id.timerSeekBar);
+        timerTextView = findViewById(R.id.timerTextView);
+        goButton = findViewById(R.id.goButton);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateTimer(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    public void resetTimer(){
+        timerTextView.setText("00:30");
+        timerSeekBar.setProgress(30);
+        timerSeekBar.setEnabled(true);
+        countDownTimer.cancel();
+        goButton.setText("GO!");
+        counterIsactive = false;
+    }
+
+    public void updateTimer(int secondsLeft){
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft - (minutes * 60);
+        String secondString = Integer.toString(seconds);
+        if (seconds <= 9){
+            secondString = "0" + secondString;
+        }
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+    }
+
+}
